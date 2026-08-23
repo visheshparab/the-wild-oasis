d97c2e7cd71d2637451c432133e688955af4fc1c
Vishesh <visheshparab848@gmail.com>
2026-08-23 16:00:14 +0530

    added styled components and basic project layout

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,19 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => sizes[props.sizes]}
+  ${(props) => variations[props.variations]}
+`;
+
+Button.defaultProps = {
+  variations: "primary",
+  sizes: "medium",
+};
+
+export default Button;
